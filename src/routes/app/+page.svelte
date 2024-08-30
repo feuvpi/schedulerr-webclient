f8c968c9dcf4bbf41d47c6c8b62db737454a4d48
feuvpi <fredvpg@gmail.com>
2024-08-29 22:23:45 -0300

    .

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import Calendar from '@event-calendar/core';
+	import TimeGrid from '@event-calendar/time-grid';
+	import ResourceTimeLine from '@event-calendar/resource-timeline';
 
+	import type { PageData } from './$types';
 	export let data: PageData;
+
+	let plugins = [TimeGrid];
+	let options = {
+		view: 'timeGriMonth',
+		events: [
+			// your list of events
+		]
+	};
 </script>
+
+<Calendar {plugins} {options} />
